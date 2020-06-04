d1f126ee0319391be666b4301ea95903eab4c175
Alaa Essam <alaaessammirah@gmail.com>
2020-06-04 18:29:03 +0200

    struct

--- reading.h
+++ reading.h
@@ -1,4 +1,22 @@
 #ifndef READING_H
 #define READING_H
 
+#include<iostream>
+#include <string>
+#include<vector>
+
+
+struct image {
+
+    unsigned int rows;
+    unsigned int cols;
+    unsigned int maxVal;
+    std::string format;
+    std::string comment;
+    std::vector <uint8_t> grayvalues;
+};
+
+image pgm_read( const std::string filepath){
+    
+}
 #endif // READING_H
