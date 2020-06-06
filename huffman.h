993b2ab328720da6086ef88b4113ec67f171f00b
Mariam Ashraf <mariam.aziz99@eng-st.cu.edu.eg>
2020-06-06 17:55:23 +0200

    huffman tree1

--- huffman.h
+++ huffman.h
@@ -1,11 +1,57 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
-
-#iclude <bitset>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include "freq_map.h"
+#include <bitset>
 #include <string>
 #include <vector>
+using namespace std;
 
+// a tree node
+struct Node
+{
+    uint8_t pixel;
+    int freq;
+    Node *left, *right;
+};
 
+// function to allocate a new node
+Node* getnode (uint8_t pixel, int freq, Node* left, Node* right)
+{
+    Node* node = new Node();
+    node->pixel = pixel;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+
+    return node;
+}
+//comparison object to be used to order the heap
+struct comp
+{
+    bool operator()(Node* l, Node* r)
+    {
+        //highest priority item has lowest frequency
+        return l->freq > r->freq;
+
+    }
+};
+//traverse the huffman tree and store huffman codes in a map
+void encode(Node* root, string str, unordered_map<uint8_t , string> &huffmanCode)
+{
+    if (root== nullptr)
+        return;
+    // found a leaf node
+    if (!root->left && !root->right)
+    {
+        huffmanCode[root->pixel] = str;
+    }
+    encode(root->left , str +"0" , huffmanCode);
+    encode(root->right , str + "1", huffmanCode);
+
+}
 
 std::string bits_string (std::vector <uint8_t> image,  unordered_map <uint8_t , string > hCode){
     std::string bitsString="";
