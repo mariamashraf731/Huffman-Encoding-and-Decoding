7512a0216d65b52ab1811de7b6cd2e43d591fa5c
nouran <nouran.youssef99@eng-st.cu.edu.eg>
2020-06-06 17:08:13 +0200

    compress.cpp:calling freq_map, buildhuffmantree,bits_string,bytes_array

--- compress.cpp
+++ compress.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include "reading.h"
+#include<unordered_map>
+#include "huffman.h"
+
 
 image pgm_read (const std::string);
 
@@ -7,5 +10,14 @@ int main(int argc, char* argv[])
 {
     image im;
     im = pgm_read (argv[1]);
+
+    std::unordered_map<uint8_t,int> Frequency = freq_map (im.pixels_values);
+
+    std::unordered_map<uint8_t,string> huffmanCode = buildhuffmanTree( Frequency);
+
+    std::string compressed_string = bits_string(im.pixels_values,huffmanCode);
+
+    std::vector <uint8_t> compressed_bytes = bytes_array(compressed_string);
+
 
 }
