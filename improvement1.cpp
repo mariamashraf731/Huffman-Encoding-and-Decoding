e85edac8a7e14e01e2306d249a5aedefa708f913
Mariam Ashraf <mariam.aziz99@eng-st.cu.edu.eg>
2020-06-09 16:40:41 +0200

    GUI Fixed

--- improvement1.cpp
+++ improvement1.cpp
@@ -18,7 +18,10 @@ std::string type_found = file_name.substr(type+1);
 
 if (type_found == "pgm")
 {
-    image im = pgm_read ("./data/"+std::string(argv[1]));
+    
+    image im;
+
+    im = pgm_read ("./data/"+std::string(argv[1]));
 
     std::map<uint8_t,int> Frequency = freq_map (im.pixels_values);
 
@@ -32,7 +35,7 @@ if (type_found == "pgm")
     //serilizing compressed image
 
     std::ofstream out;
-    out.open("./compressed/encoded.enc", std::ios::out | std::ios::binary);
+    out.open("./compressed/image.enc", std::ios::out | std::ios::binary);
     //data
     out<<im.format<<std::endl;
     out<<im.cols<<" "<<im.rows<<std::endl;
@@ -46,19 +49,26 @@ if (type_found == "pgm")
     }
 
     //compressed array
-    unsigned long i=0;
-    for(auto byte:compressed_bytes ){
-        if(i!=compressed_bytes.size()){
-            out<<byte;
-            i++;
-        }
+    unsigned int i = 0;
+
+    for (auto byte : compressed_bytes )
+    {
+        out << byte;
+        if (++i == im.cols)
+            out<< std::endl;
     }
 
+
+
+
+
     out.close();
+
 } 
 else if (type_found == "enc")
   {
 
+   
     deserillized compressed_data= deserilize("./compressed/"+std::string(argv[1]));
     std::vector <uint8_t> decoded = decode( compressed_data.freq_map, &compressed_data.bits_string);
 
@@ -74,14 +84,12 @@ else if (type_found == "enc")
 
     for (auto d : decoded)
     {
-       image_out<<d;
+        image_out<<d;
         if (++i == compressed_data.cols)
             image_out<< std::endl;
     }
 
     image_out.close();
 
-   }
-   
    return 0;
 }
